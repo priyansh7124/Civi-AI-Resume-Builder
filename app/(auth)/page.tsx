ffee4b088e0ec4f6cb870929c9a0da9b040a8e81
Priyansh Agrawal <priyanshagrawal806@gmail.com>
2025-12-25 01:36:23 +0530

    Changed the ui in the dashboard

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
               </span>
             </Link>
             <Link
-              href="https://github.com/exclusiveabhi/ai-resume-builder.git"
+              href="https://github.com/priyansh7124/Civi-AI-Resume-Builder"
               className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-slate-200 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
             >
               <span className="relative text-base font-semibold text-primary">
@@ -115,9 +115,9 @@ const page = () => {
             </span>
             . All Rights Reserved.
           </span>
-          <Link href="https://github.com/exclusiveabhi" className="me-4 md:me-6">
+          <Link href="https://github.com/priyansh7124" className="me-4 md:me-6">
             <span className="hover:text-primary-500 mt-3 text-sm font-medium text-gray-500 sm:mt-0">
-              Made with ❤️ by Abhishek
+              Made with ❤️ by Priyansh
             </span>
           </Link>
         </div>
